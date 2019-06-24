aed4b9a26a04990ad4d1ab52322f905035aa3894
Will <fgnickson@gmail.com>
2019-06-24 16:44:18 +0100

    added cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const apiRouter = require("./routes/api");
 const { handle500, handle404, handle400 } = require("./errors");
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
